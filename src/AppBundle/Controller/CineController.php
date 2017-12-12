ccba0b34835fcb7a45cc10cb7e39ef19de51ee8e
Miguel Ángel Durán Lucha <maduranlucha@gmail.com>
2017-12-12 10:00:17 +0100

    Añadida Lista de peliculas de diciembre

--- src/AppBundle/Controller/CineController.php
+++ src/AppBundle/Controller/CineController.php
@@ -13,6 +13,17 @@ class CineController extends Controller
 
     public function listarAction()
     {
-        return $this->render('cineworld/mostrar.html.twig');
+        $peliculas = [
+
+            ['titulo' =>'Armageddon','director' => 'Michael Bay','genero' => 'Ciencia Ficcion','duracion' => '150 Minutos', 'calidad' => 'DVD','imagen' => 'img/armageddon.jpg'],
+            ['titulo' =>'Salvar al soldado Ryan','director' => 'Steven Spilverg','genero' => 'Belico','duracion' => '169 Minutos', 'calidad' => 'BlueRay', 'imagen' => 'img/ryan.jpg'],
+            ['titulo' =>'La lista de Schindler','director' => 'Steven Spilverg','genero' => 'Drama','duracion' => '196 Minutos', 'calidad' => 'DVD', 'imagen' => 'img/lista.jpg'],
+            ['titulo' =>'Terminator','director' => 'James Cameron','genero' => 'Ciencia Ficcion','duracion' => '108 Minutos', 'calidad' => 'UHD', 'imagen' => 'img/terminator.jpg']
+
+        ];
+
+        return $this->render('cineworld/mostrar.html.twig',[
+            'peliculas' => $peliculas
+        ]);
     }
 }
