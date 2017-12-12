a0726cf0deede2a3243bb8700f8787c313830185
Miguel Ángel Durán Lucha <maduranlucha@gmail.com>
2017-12-12 10:22:27 +0100

    Correccion de una pelicula

--- src/AppBundle/Controller/CineController.php
+++ src/AppBundle/Controller/CineController.php
@@ -11,7 +11,7 @@ class CineController extends Controller
      * @Route("/cine", name="cinemostrar")
      */
 
-    public function listarAction()
+    public function cineMostrar()
     {
         $peliculas = [
 
